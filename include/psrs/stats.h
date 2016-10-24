9ec5fc2989d59393d39dd66c533885c8e5025190
Jiahui Xie <jiahui.xie@outlook.com>
2016-10-23 19:47:52 -0600

    Rewrite moving average calculation functions
    - Add a new 'moving_window' structure used for moving average calculation
    - Change the signature of 'moving_average_calc' function to make it operate on
    'moving_window' structure instead
    - Implement 'moving_window_init', 'moving_window_push', and
    'moving_window_destroy' functions to manipulate 'moving_window' structure

--- include/psrs/stats.h
+++ include/psrs/stats.h
@@ -3,7 +3,24 @@
 
 #include "psrs/ring.h"
 
-int moving_average_push(struct ring *window, const double value);
-int moving_average_calc(const struct ring *window, double *average);
+/*
+ * 'moving_window' is a specialized version of 'ring': it has an extra
+ * 'written' field and all the values are of double type.
+ */
+struct moving_window {
+        struct ring *ring;
+        /*
+         * This field is only meaningful when there is no overflow;
+         * once an overflow is about to occur, it would stay at 'SIZE_MAX'
+         * for all the following write(s).
+         */
+        size_t written;
+};
+
+int moving_window_init(struct moving_window **self, const size_t length);
+int moving_window_push(struct moving_window *self, const double value);
+int moving_window_destroy(struct moving_window **self);
+
+int moving_average_calc(const struct moving_window *window, double *average);
 
 #endif /* STATS_H */
--- src/stats.c
+++ src/stats.c
@@ -1,21 +1,80 @@
 #include "psrs/stats.h"
 
 #include <errno.h>
+#include <stdlib.h>
 
-int moving_average_push(struct ring *window, const double value)
+int moving_window_init(struct moving_window **self, const size_t length)
 {
-        if (NULL == window) {
+        struct moving_window *window = NULL;
+        struct ring *ring = NULL;
+
+        if (NULL == self || 0U == length) {
                 errno = EINVAL;
                 return -1;
         }
 
-        if (0 > ring_add(window, &value, sizeof(double))) {
+        window = (struct moving_window *)malloc(sizeof(struct moving_window));
+
+        if (NULL == window) {
                 return -1;
+        }
+
+        if (0 > ring_init(&ring, length, NULL, NULL)) {
+                free(window);
+                return -1;
+        }
+
+        window->ring = ring;
+        window->written = 0U;
+
+        *self = window;
+        return 0;
+}
+
+int moving_window_push(struct moving_window *self, const double value)
+{
+        if (NULL == self) {
+                errno = EINVAL;
+                return -1;
+        }
+
+        if (0 > ring_add(self->ring, &value, sizeof(double))) {
+                return -1;
+        }
+
+        /*
+         * Let the 'written' field stay at 'SIZE_MAX' if it is about to
+         * overflow: this field is only used for testing inequality
+         * between itself and the 'length' of ring in 'moving_average_calc'.
+         */
+        if (0U != ~(self->written)) {
+                self->written++;
         }
         return 0;
 }
 
-int moving_average_calc(const struct ring *window, double *average)
+int moving_window_destroy(struct moving_window **self)
+{
+        struct moving_window *window = NULL;
+
+        if (NULL == self) {
+                errno = EINVAL;
+                return -1;
+        }
+
+        window = *self;
+
+        if (0 > ring_destroy(&(window->ring))) {
+                return -1;
+        }
+
+        free(window);
+
+        *self = NULL;
+        return 0;
+}
+
+int moving_average_calc(const struct moving_window *window, double *average)
 {
         double result = 0;
         double *current = NULL;
@@ -31,22 +90,34 @@ int moving_average_calc(const struct ring *window, double *average)
          * Since both 'window' and '&window_size' cannot be NULL at this point,
          * so this function call must succeed.
          */
-        ring_length(window, &window_size);
+        ring_length(window->ring, &window_size);
 
-        if (0 > ring_iter_init(&iter, window)) {
+        /*
+         * If the number of elements 'written' in total is less than
+         * the actual 'window_size', moving average can not be calculated.
+         * For example, if the 'window_size' is 4, and there are only 2 numbers
+         * available in the window (ring), the rest of 2 buffers in window
+         * (ring) would be empty.
+         */
+        if (window->written < window_size) {
+                errno = ENOTSUP;
+                return -1;
+        }
+
+        if (0 > ring_iter_init(&iter, window->ring)) {
                 return -1;
         }
 
         for (size_t i = 0U; i < window_size; ++i) {
+                /* Both arguments can not be NULL; return value unchecked. */
                 ring_iter_walk(iter, (void **)(&current));
                 result += *current;
         }
 
         result /= window_size;
 
-        if (0 > ring_iter_destroy(&iter)) {
-                return -1;
-        }
+        /* 'iter' can not be NULL; return value unchecked. */
+        ring_iter_destroy(&iter);
 
         *average = result;
         return 0;
